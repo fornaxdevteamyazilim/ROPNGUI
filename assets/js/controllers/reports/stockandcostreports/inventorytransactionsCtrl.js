bf6abbc9a2168cb0a5e23b924e430d23c50cfe94
fornaxdevteamyazilim <fornaxdevteam@gmail.com>
2020-07-13 07:47:02 +0300

    InvTran  report düzenlemeleri

--- assets/js/controllers/reports/stockandcostreports/inventorytransactionsCtrl.js
+++ assets/js/controllers/reports/stockandcostreports/inventorytransactionsCtrl.js
@@ -1,15 +1,15 @@
 ﻿'use strict';
 app.controller('inventorytransactionsCtrl', inventorytransactionsCtrl);
-function inventorytransactionsCtrl($scope, $log, $modal, $filter, SweetAlert, Restangular, ngTableParams, toaster, $window, $stateParams, $rootScope, $location, $timeout, Excel, $translate, userService, ngnotifyService, $element, NG_SETTING, $http, $q) {
+function inventorytransactionsCtrl($scope, $log, $modal, $filter, SweetAlert, Restangular, ngTableParams, toaster, $window, $stateParams, $rootScope, $location, $timeout, Excel, $translate, userService, ngnotifyService, $element, NG_SETTING, $http, $q, localStorageService) {
     $rootScope.uService.EnterController("inventorytransactionsCtrl");
     $scope.Time = ngnotifyService.ServerTime();
-    
+
     if ($rootScope.user.userstores && $rootScope.user.userstores.length > 1) {
         $scope.selectStore = true;
         $scope.StoreID = '';
     }
     else {
-        $scope.StoreID=$rootScope.user.StoreID;
+        $scope.StoreID = $rootScope.user.StoreID;
     }
     $scope.Back = function () {
         $window.history.back();
@@ -110,30 +110,86 @@ function inventorytransactionsCtrl($scope, $log, $modal, $filter, SweetAlert, Re
         columns: [
             //{ dataField: "id", dataType: "number"},
             { dataField: "TransactionDate", dataType: "Date" },
-            { dataField: "Inventory.ItemCode", caption: "Code",dataType: "string" },
+            { dataField: "Inventory.ItemCode", caption: "Code", dataType: "string" },
             { dataField: "Inventory.name", caption: "Inventory", dataType: "string" },
-            { dataField: "InventoryUnit.name", caption: "UnitName",dataType: "string" },
+            { dataField: "InventoryUnit.name", caption: "UnitName", dataType: "string" },
             { caption: "Units", dataField: "UnitCount", dataType: "number", format: { type: "fixedPoint", precision: 2 } },
             { caption: "Price", dataField: "UnitPrice", dataType: "number", format: { type: "fixedPoint", precision: 2 } },
             { caption: "Amount", dataField: "Amount", dataType: "number", format: { type: "fixedPoint", precision: 2 } },
             { dataField: "Repository", dataType: "string" },
-            { dataField: "Store", dataType: "string" },
+            //{ dataField: "Store", dataType: "string" },
+            {
+                dataField: "StoreID", caption: "Store",
+                lookup: {
+                    valueExpr: "id",
+                    displayExpr: "Store",
+                    dataSource: {
+                        store: DevExpress.data.AspNet.createStore({
+                            key: "id",
+                            loadUrl: NG_SETTING.apiServiceBaseUri + "/api/dxStoreWithRegions",
+                            onBeforeSend: function (method, ajaxOptions) {
+                                var authData = localStorageService.get('authorizationData');
+                                if (authData) {
+                                    ajaxOptions.headers = {
+                                        Authorization: 'Bearer ' + authData.token,
+                                        'Content-type': 'application/json'
+                                    };
+                                }
+                            }
+                        }),
+                        sort: "Store",
+                        headerFilter: { allowSearch: true }
+                    },
+                    calculateSortValue: function (data) {
+                        var value = this.calculateCellValue(data);
+                        return this.lookup.calculateCellValue(value);
+                    }
+                },
+            },
+            {
+                dataField: "StoreID", caption: "StoreType",
+                lookup: {
+                    valueExpr: "id",
+                    displayExpr: "StoreType",
+                    dataSource: {
+                        store: DevExpress.data.AspNet.createStore({
+                            key: "id",
+                            loadUrl: NG_SETTING.apiServiceBaseUri + "/api/dxStoreWithRegions",
+                            onBeforeSend: function (method, ajaxOptions) {
+                                var authData = localStorageService.get('authorizationData');
+                                if (authData) {
+                                    ajaxOptions.headers = {
+                                        Authorization: 'Bearer ' + authData.token,
+                                        'Content-type': 'application/json'
+                                    };
+                                }
+                            }
+                        }),
+                        sort: "StoreType",
+                        headerFilter: { allowSearch: true }
+                    },
+                    calculateSortValue: function (data) {
+                        var value = this.calculateCellValue(data);
+                        return this.lookup.calculateCellValue(value);
+                    }
+                },
+            },
             { dataField: "TransactionType", dataType: "string" },
             { dataField: "TransactionDetail", dataType: "string" },
             { dataField: "InventoryGroup", dataType: "string" },
         ],
         summary: {
             totalItems: [{ column: "Inventory.name", summaryType: "count", displayFormat: "{0}" },
-                { column: "Units", summaryType: "sum", valueFormat: { type: "fixedPoint", precision: 2 }, displayFormat: "{0}" },
-                { column: "Amount", summaryType: "sum", valueFormat: { type: "fixedPoint", precision: 2 }, displayFormat: "{0}₺" },        
+            { column: "Units", summaryType: "sum", valueFormat: { type: "fixedPoint", precision: 2 }, displayFormat: "{0}" },
+            { column: "Amount", summaryType: "sum", valueFormat: { type: "fixedPoint", precision: 2 }, displayFormat: "{0}₺" },
             ],
-        },        
+        },
         export: {
             enabled: true,
-            fileName: "InventoryTransactions",        
+            fileName: "InventoryTransactions",
         },
         scrolling: { mode: "virtual" },
-        height: 600        
+        height: 600
     };
     $scope.selectBox = {
         dataSourceUsage: {
@@ -148,7 +204,7 @@ function inventorytransactionsCtrl($scope, $log, $modal, $filter, SweetAlert, Re
             bindingOptions: {
                 value: "StoreID"
             }
-        },        
+        },
     };
     $scope.LoadData = function () {
         var dataGrid = $('#gridContainer').dxDataGrid('instance');
