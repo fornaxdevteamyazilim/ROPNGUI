ee734bad5b83721346a66f3c02a2f93b7a8657d4
fornaxdevteamyazilim <fornaxdevteam@gmail.com>
2021-02-08 16:13:42 +0300

    V 655

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,7 +95,7 @@ app.run(['$rootScope', '$state', '$browser', '$stateParams', '$location', 'authS
             name: 'ROP NG',
             author: 'Fornax A.Ş.',
             description: 'Retail Operation Platform NG',
-            version: '1.0.654',
+            version: '1.0.655',
             year: ((new Date()).getFullYear()),
             isMobile: (function () {// true if the browser is a mobile device
                 var check = false;
@@ -132,7 +132,7 @@ app.config(function ($httpProvider) {
                 if (request.url.endsWith(".html") && !request.url.includes("tabset.html")) {
                     if ($templateCache.get(request.url) === undefined) { // cache miss
                         // Item is not in $templateCache so add our query string
-                        request.url = request.url + '?v=0654';
+                        request.url = request.url + '?v=0655';
                     }
                 }
                 return request;
@@ -145,9 +145,9 @@ app.run(['authService', function (authService) {
 }]);
 app.run(['userService', function (userService) { }]);
 //app.value('signalRServer', 'http://10.101.252.149:9065');//Little Caesars
-app.value('signalRServer', 'http://192.168.9.40:9065');//PH
+//app.value('signalRServer', 'http://192.168.9.40:9065');//PH
 //app.value('signalRServer', 'http://10.0.0.245:9065');//MAROCCO
-//app.value('signalRServer', 'http://localhost:9065');//localhost
+app.value('signalRServer', 'http://localhost:9065');//localhost
 app.run(['callsService', function (callsService) { }]);
 app.run(['ngnotifyService', function (ngnotifyService) { }]);
 // translate config
